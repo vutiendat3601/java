9e72fd9bf6dfa27501088e5aa2661025d4459681
vutiendat3601 <vutien.dat.3601@gmail.com>
2023-12-10 13:43:55 +0700

    jar

---
+++ jar/Main.java
@@ -0,0 +1,8 @@
+package jar;
+
+public class Main {
+    public static void main(String[] args) {
+        FloatNumber number = new FloatNumber();
+        System.out.println(number.add(1, 2.3D));
+    }
+}
